0b429bfe923ddd4d4bab3bc36d4aa7836d7216c8
MG1845 <metegencdogan@gmail.com>
2020-12-04 00:48:30 +0100

    preAuthorize annotation was added instead of some antMatchers

--- src/main/java/com/example/springsecuritylesson1/student/StudentManagementController.java
+++ src/main/java/com/example/springsecuritylesson1/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.springsecuritylesson1.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,21 +17,25 @@ public class StudentManagementController {
 
 
     @GetMapping("management/api/v1/students")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping("management/api/v1/students/")
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(student);
     }
 
     @DeleteMapping("management/api/v1/students/{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(studentId);
     }
 
     @PutMapping("management/api/v1/students/{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println(String.format("%s %s", studentId, student));
     }
